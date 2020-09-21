8d6592e8cb8280caf2c81f3f5866ec9b00079234
Akshay Havale <akshayhavale22@gmail.com>
2020-09-21 18:28:57 +0530

    final-setup

--- person-project/src/main/java/com/user/controller/EmployeeController.java
+++ person-project/src/main/java/com/user/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.user.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +21,26 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@PostMapping
+	@PostMapping("/create")
 	public @ResponseBody Person create(@RequestBody Person emp) {
 		return employeeService.createEmployee(emp);
+	}
+	
+	@PutMapping("/update/{id}")
+	public @ResponseBody Person update(@RequestBody Person emp,@PathVariable(name = "id") long id) {
+		return employeeService.updateEmployee(emp, id);
+	}
+	@GetMapping("/get/{id}")
+	public @ResponseBody Person get(@PathVariable(name = "id") long id) {
+		return employeeService.getEmployee(id);
+	}
+	@GetMapping("/get/all")
+	public @ResponseBody List<Person> getAll() {
+		return employeeService.getAllEmployee();
+	}
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable(name = "id") long id) {
+		 employeeService.deletePerson( id);
 	}
 
 }
